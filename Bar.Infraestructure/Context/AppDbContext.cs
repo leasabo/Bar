2b81f0728609553e491a3d3fe93a1ba754e4d47d
Leandro Sabo <lu1gaq.ar@gmail.com>
2024-08-19 19:12:20 -0300

    Crear prueba de AppDbContext para la capa de Infra

---
+++ Bar.Infraestructure/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Bar.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bar.Infraestructure.Context
+{
+    public class AppDbContext:DbContext
+    {
+        // Constructor para inicializar el contexto de la BBDD y permite
+        // inyección de dependencias aceptando opciones como parámetro
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { } 
+        
+        // DBSet es una clase en Entity Framework que
+        // representa una colección de entidades en BBDD
+        public DbSet<Waiter> Waiters { get; set; }
+    }
+}
