3db0b2535909577ee96a96895d08bd30c230abd3
Leandro Sabo <lu1gaq.ar@gmail.com>
2024-08-20 02:20:31 -0300

    Desinstalar paquete Nuget que generaba conflicto

---
+++ Bar.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Bar.Domain.Entities;
+using Bar.Application.Commands;
+
+namespace Bar.Application.Profiles
+{ 
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Configura el mapeo entre CreateWaiterCommand y Waiter
+            CreateMap<CreateWaiterCommand, Waiter>();
+            // Configura el mapeo entre CreateTableCommand y Table
+            CreateMap<CreateTableCommand, Table>();
+            // Configura el mapeo entre CreateReceiptCommand y Receipt
+            CreateMap<CreateReceiptCommand, Receipt>();
+            // Agrega más configuraciones de mapeo según sea necesario
+        }
+    }
+}
--- Bar.Web/Program.cs
+++ Bar.Web/Program.cs
@@ -3,17 +3,15 @@ using Bar.Domain.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Bar.Infraestructure.Context;
-using Microsoft.EntityFrameworkCore;
-
-
+using AutoMapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// Registrarr AutoMapper
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+// Registrar AutoMapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies()); 
 
 // Conexión a la BBDD
 var connectionString = builder.Configuration.GetConnectionString("Connection");
@@ -28,17 +26,14 @@ builder.Services.AddScoped<IWaiterRepository, WaiterRepository>();
 builder.Services.AddScoped<ITableRepository, TableRepository>();
 builder.Services.AddScoped<IReceiptRepository, ReceiptRepository>();
 
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
