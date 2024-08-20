0959c385ccb90e2068c69b2238e24a633f31544c
Leandro Sabo <lu1gaq.ar@gmail.com>
2024-08-20 19:53:25 -0300

    Agregar verbos al controlador Mesas

--- Bar.Web/Controllers/TablesController.cs
+++ Bar.Web/Controllers/TablesController.cs
@@ -1,13 +1,17 @@
-﻿using Bar.Domain.Entities;
+﻿using Bar.Application.Commands.CreateTable;
+using Bar.Application.Commands.DeleteTable;
+using Bar.Application.Commands.UpdateTable;
+using Bar.Application.Queries.GetTable;
+using Bar.Application.Queries.GetTableList;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
-using Bar.Application.Commands;
 
 namespace Bar.Web.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/v1/tables")]
+    [Consumes("application/json")]
+    [Produces("application/json")]
     public class TablesController : ControllerBase
     {
         private readonly IMediator _mediator;
@@ -17,13 +21,51 @@ namespace Bar.Web.Controllers
             _mediator = mediator;
         }
 
-        [HttpPost]
-        public async Task<IActionResult> CreateTable(CreateTableCommand command)
+        // GET: api/Tables
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetList()
         {
-            var result = await _mediator.Send(command);
-            return Ok(result);
+            var response = await _mediator.Send(new GetTableListQuery());
+            return Ok(response);
         }
 
-        // Similar endpoints for Get, Update, Delete operations
+        // GET: api/Tables/5
+        [HttpGet]
+        [Route("{tableId:int}")]
+        public async Task<IActionResult> Get([FromRoute] int tableId)
+        {
+            var response = await _mediator.Send(new GetTableQuery { TableId = tableId });
+            return Ok(response);
+        }
+
+        // POST: api/Tables
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> Create([FromBody] CreateTableCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok();
+        }
+
+        // PUT: api/Tables/5
+        [HttpPut]
+        [Route("{tableId:int}")]
+        public async Task<IActionResult> Update([FromRoute] int tableId, [FromBody] UpdateTableCommand command)
+        {
+            command.TableId = tableId;
+            await _mediator.Send(command);
+            return Ok();
+        }
+
+        // DELETE: api/Tables/5
+        [HttpDelete]
+        [Route("{tableId:int}")]
+        public async Task<IActionResult> Delete([FromRoute] int tableId)
+        {
+            var command = new DeleteTableCommand { TableId = tableId };
+            await _mediator.Send(command);
+            return Ok();
+        }
     }
 }
